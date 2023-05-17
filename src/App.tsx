800d3c920e788f77da8df458fae6fafdee9ddfef
USER <jsdev04@gmail.com>
2023-05-17 15:08:18 +0600

    Обновление App.tsx отображения компонента Ball

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Ball from './Ball/Ball';
 
-const App = () => {
+const App: React.FC = () => {
+  const [numbers, setNumbers] = useState<number[]>([]);
+
+  const generateNumbers = () => {
+    const newNumbers: number[] = [];
+    while (newNumbers.length < 5) {
+      const randomNumber = Math.floor(Math.random() * 32) + 5;
+      if (!newNumbers.includes(randomNumber)) {
+        newNumbers.push(randomNumber);
+      }
+    }
+    setNumbers(newNumbers.sort((a, b) => a - b));
+  };
+
   return (
     <div className="App">
-     
+      <h1>Lottery Number Generator</h1>
+      <button onClick={generateNumbers}>New numbers</button>
+      <div className="balls">
+        {numbers.map((number, index) => (
+          <Ball key={index} number={number} />
+        ))}
+      </div>
     </div>
   );
 };
